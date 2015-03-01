54079e137006dcb523e3f807b53590ae4cec2346
Johan Kjölhede <gigurra@gmail.com>
2015-03-01 22:33:30 +0100

    Adding mouse/keyboard input buffers

--- src/main/scala/se/gigurra/wallace/client/ClientMain.scala
+++ src/main/scala/se/gigurra/wallace/client/ClientMain.scala
@@ -3,24 +3,49 @@ package se.gigurra.wallace.client
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
+
+import com.jogamp.newt.event.KeyAdapter
+import com.jogamp.newt.event.KeyEvent
+import com.jogamp.newt.event.KeyListener
+import com.jogamp.newt.event.MouseAdapter
+import com.jogamp.newt.event.MouseEvent
+import com.jogamp.newt.event.MouseListener
 import com.jogamp.newt.opengl.GLWindow
+
 import javax.media.opengl.GLCapabilities
 import javax.media.opengl.GLProfile
 import se.gigurra.renderer.Renderer
 import se.gigurra.renderer.Widget
 import se.gigurra.renderer.glimpl.GlRenderer.Attachable
 import se.gigurra.renderer.glimpl.GlRenderer2d
+import se.gigurra.util.CallBuffer
+import se.gigurra.util.CallBuffer.toInterface
 import se.gigurra.util.Mutate.Mutable
 import se.gigurra.wallace.GameState
-import com.jogamp.newt.event.MouseListener
 
-class ClientMain extends Widget[ClientMain] {
-
+class ClientMain(
+  val keyboardInput: CallBuffer[KeyListener],
+  val mouseInput: CallBuffer[MouseListener]) extends Widget[ClientMain] {
   val gamestate = new GameState
   val ingameGraphics = new IngameGraphics
   val serverConnection = new ServerConnection
 
+  val mouseHandler = new MouseAdapter {
+    override def mouseMoved(e: MouseEvent) {
+      println(s"Moved mouse: $e")
+    }
+  }
+
+  val keyHandler = new KeyAdapter {
+    override def keyPressed(e: KeyEvent) {
+      println(s"Pressed key: $e")
+    }
+  }
+
   override def doDraw(renderer: Renderer, isRoot: Boolean) {
+
+    mouseInput.flush(mouseHandler)
+    keyboardInput.flush(keyHandler)
 
     ingameGraphics.draw(renderer, gamestate.localView.world())
 
@@ -54,13 +79,14 @@ object ClientMain {
       glWindow.setPointerVisible(true)
       glWindow.setVisible(true)
     }
-    
-    /*
-    glWindow.addMouseListener(new MouseListener {
-      
-    })*/
 
-    glWindow.attach(new ClientMain, new GlRenderer2d(_))
+    val keyboardInput = new CallBuffer[KeyListener]
+    val mouseInput = new CallBuffer[MouseListener]
+
+    glWindow.addKeyListener(keyboardInput)
+    glWindow.addMouseListener(mouseInput)
+
+    glWindow.attach(new ClientMain(keyboardInput, mouseInput), new GlRenderer2d(_))
 
   }
 }
