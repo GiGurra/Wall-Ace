63c527a3feb9306b14e5d0e3df70454e110e8668
Johan Kjölhede <kjolhede@telia.com>
2015-10-18 14:24:44 +0200

    Change to a libgdx sbt project :)

---
+++ desktop/src/main/scala/se/gigurra/wallace/Main.scala
@@ -0,0 +1,12 @@
+package se.gigurra.wallace
+
+import com.badlogic.gdx.backends.lwjgl.{LwjglApplication, LwjglApplicationConfiguration}
+
+object Main extends App {
+    val cfg = new LwjglApplicationConfiguration
+    cfg.title = "wall-ace"
+    cfg.height = 480
+    cfg.width = 800
+    cfg.forceExit = false
+    new LwjglApplication(new Wallace, cfg)
+}
