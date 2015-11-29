3b0edea53b5450013326b4e5b2eece213ae92f35
Johan Kjölhede <kjolhede@telia.com>
2015-11-29 01:49:13 +0100

    Better event handling

--- game_model/src/main/scala/se/gigurra/wallace/gamemodel/WorldUpdate.scala
+++ game_model/src/main/scala/se/gigurra/wallace/gamemodel/WorldUpdate.scala
@@ -1,5 +1,7 @@
 package se.gigurra.wallace.gamemodel
 
+import se.gigurra.wallace.util.Decorated
+
 trait WorldUpdate {
 
   def emit(event: WorldEvent)
@@ -9,3 +11,26 @@ trait WorldUpdate {
 
   def apply(world: World[_])(implicit eventReceiver: WorldEventReceiver)
 }
+
+object WorldUpdate {
+
+  case class RichWorld(state: World[_], eventReceiver: WorldEventReceiver) extends Decorated[World[_]](state)
+
+  def apply(f: (World[_], WorldEventReceiver) => Unit): WorldUpdate = {
+    new WorldUpdate {
+      override def apply(world: World[_])(implicit eventReceiver: WorldEventReceiver): Unit = {
+        f(world, eventReceiver)
+      }
+    }
+  }
+
+  def apply(f: RichWorld => Unit): WorldUpdate = {
+    new WorldUpdate {
+      override def apply(world: World[_])(implicit eventReceiver: WorldEventReceiver): Unit = {
+        f(RichWorld(world, eventReceiver))
+      }
+    }
+  }
+
+}
+
